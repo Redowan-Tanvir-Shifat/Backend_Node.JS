1a6cd43d99089c1cd6ef8ac85e9dd9ff119033ba
Redowan0019 <146367158+Redowan0019@users.noreply.github.com>
2025-02-15 21:48:40 +0600

    NodeJS Installation and VS Code setup

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('Hello World!');
+
+const _ = require('lodash');
+console.log(_.camelCase('Hello World!'));
+console.log(_.upperCase('Hello World!'));
+console.log(_.lowerCase('Hello World!'));
