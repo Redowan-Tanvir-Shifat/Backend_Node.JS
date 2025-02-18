c1d2d39e096188673d7af487afc87f9f8961f624
Redowan0019 <146367158+Redowan0019@users.noreply.github.com>
2025-02-18 23:26:23 +0600

    Stream Read &Write (Piping)

--- Lesson_6/index.js
+++ Lesson_6/index.js
@@ -1,10 +1,29 @@
 // <-----------------------------Stream & Buffer----------------------------->
 // <--------------Write Stream-------------->
-const fs = require('fs');
-const ourReadStream = fs.createReadStream(`${__dirname}/bigData.txt`);
+// const fs = require('fs');
+// const ourReadStream = fs.createReadStream(`${__dirname}/bigData.txt`);
 
-const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
+// const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
 
-ourReadStream.on('data', (chunk) => {
-    ourWriteStream.write(chunk);
+// // ourReadStream.on('data', (chunk) => {
+// //     ourWriteStream.write(chunk);
+// // });
+
+
+
+// // <--------------Piping-------------->
+// ourReadStream.pipe(ourWriteStream);
+
+
+
+// <--------------Overview-------------->
+const http = require('http');
+const fs  = require('fs');
+
+const server = http.createServer((req, res) => {
+   const myReadStream = fs.createReadStream(`${__dirname}/bigData.txt`, 'utf8');
+   myReadStream.pipe(res);
 });
+
+server.listen(3000); // listen to port 3000
+console.log('Server is running on port 3000');
