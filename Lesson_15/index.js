c7842b407efa3d3dc7529231347234af29834b15
Redowan0019 <146367158+Redowan0019@users.noreply.github.com>
2025-02-25 21:26:37 +0600

    Introduction Express.js

---
+++ Lesson_15/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('This is home page.');
+});
+
+app.post('/', (req, res) => {
+    res.send('This is home page with post request.');
+});
